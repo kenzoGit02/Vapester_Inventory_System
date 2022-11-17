96ea9ad82fd11e244d77dd4c609f2d283718843b
Whackyyyyy <100836755+Whackyyyyy@users.noreply.github.com>
2022-11-17 12:05:15 +0800

    Loginstuff

---
+++ inventory_management_system/Controllers/Login1Controller.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using inventory_management_system.Models;
+using System.Data;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
+
+namespace inventory_management_system.Controllers
+{
+    public class Login1Controller : Controller
+    {
+        // GET: Login1
+        public ActionResult Index()
+        {
+            List<Login> list1 = new List<Login>();
+            string mainconn = ConfigurationManager.ConnectionStrings["phpMyAdminConnection"].ConnectionString;
+            MySqlConnection mysql = new MySqlConnection(mainconn);
+            string query = "select * from tbl_accounts ";
+            MySqlCommand comm = new MySqlCommand(query);
+            comm.Connection = mysql;
+            mysql.Open();
+            MySqlDataReader dr = comm.ExecuteReader();
+            while (dr.Read())
+            {
+                list1.Add(new Login
+                {
+                   Email = dr["Email"].ToString(),
+                   Password = dr["Password"].ToString(),
+
+                });
+            }
+            mysql.Close();
+            return View(list1);
+        }
+    }
+}
