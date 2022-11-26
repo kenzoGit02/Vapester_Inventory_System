e8d4e41380b9983a0f9e519bab2ecf29ba72a79c
davidceron17 <johndavid.ceron117@gmail.com>
2022-11-26 17:32:42 +0800

    Login

---
+++ inventory_management_system/Controllers/AccountController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using inventory_management_system.Models;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Configuration;
+
+namespace inventory_management_system.Controllers
+{
+    public class AccountController : Controller
+    {
+        string mainconn = ConfigurationManager.ConnectionStrings["phpMyAdminConnection"].ConnectionString;
+        MySqlConnection mysqlconn = new MySqlConnection();
+        MySqlCommand sqlcomm = new MySqlCommand();
+        MySqlDataAdapter mda = new MySqlDataAdapter();
+        // GET: Account
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Verify(Account acc)
+        {
+            string mainconn = ConfigurationManager.ConnectionStrings["phpMyAdminConnection"].ConnectionString;
+            MySqlConnection mysqlconn = new MySqlConnection(mainconn);
+            mysqlconn.Open();
+            string sqlquery = "select * from tbl_login where username='" + acc.Name + "' and password='" + acc.Password + "'";
+            MySqlCommand sqlcomm = new MySqlCommand(sqlquery, mysqlconn);
+            MySqlDataReader dr = sqlcomm.ExecuteReader();
+
+            if (dr.Read())
+            {
+                mysqlconn.Close();
+                return RedirectToAction("Index", "Dashboard");
+            }
+
+
+
+            else
+            {
+                mysqlconn.Close();
+                return View("Error");
+            }
+
+        }
+
+    }
+}
